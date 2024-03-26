f9fdefcbb1549638a2b6d30c615b08f7f1dfe57f
Thornthan Lerdhirunwong <thornthantham@gmail.com>
2024-03-26 13:49:46 +0700

    admin

---
+++ src/db/models/Campgrounds.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose"
+
+
+const CampgroundSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,'Please add a name'],
+        unique: true,
+        trim:true,
+        maxlenght:[50,'Name can not be more than 50 characters']
+        },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+        },
+    tel:{
+        type: String,
+        required: [true,'Please add a tel. number'],
+        match:[
+            /^(()?\d{3}())?(-|\s)?\d{3}(-|\s)\d{4}$/,
+            'Please add a valid telephone number'
+        ]
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please add a price'],
+        min: [0, 'Price must be a positive number']
+    },
+    rating: {
+        type: Number,
+        min: [1, 'Rating must be between 1 and 5'],
+        max: [5, 'Rating must be between 1 and 5']
+    }
+}, {
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+});
+
+const Campground = mongoose.models.Campground || mongoose.model("Campgrounds",CampgroundSchema)
+export default Campground
--- src/libs/getHospitals.tsx
+++ src/libs/getHospitals.tsx
@@ -1,8 +1,10 @@
+import Campground from "@/db/models/Campgrounds";
+
 async function getHospitals() {
 
     await new Promise((resolve) => setTimeout(resolve, 1000))
 
-    const response = await fetch('https://presentation-day-1-duckdaan.vercel.app/api/v1/campgrounds');
+    const response = await fetch('https://vaccine-app-backend.vercel.app/api/v1/hospitals',{next: {tags:['campgrounds']}});
     if(!response.ok) {
         throw new Error('failed to fetch');
     }
