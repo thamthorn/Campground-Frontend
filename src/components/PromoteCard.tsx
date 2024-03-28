f062de1a4d5047255e590b39b16329ca3b4c4e63
Pongsapat Methasate <pongsapatmethasate@gmail.com>
2024-03-28 22:32:33 +0700

    fix rating

--- src/components/PromoteCard.tsx
+++ src/components/PromoteCard.tsx
@@ -23,8 +23,11 @@ function PromoteCard() {
         {/* <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3
         py-2 text-white showdow-sm m-6"
         onClick={() => setPlaying(!playing)}>{ playing? 'Pause' : 'Play' }</button> */}
-        <Rating className="w-auto h-[10%]" value={(rating==undefined ? 0: rating)}
+        <div className="w-auto">
+        <Rating className="w-[auto] h-full" value={(rating==undefined ? 0: rating)}
         onChange={(e, newValue) => {if(newValue!= null) setRating(newValue)}} size="large"/>
+        </div>
+        
     </div>
      
     </div>
